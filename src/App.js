fac874536c919d00d76174678f1bd405d725511e
Shestakov Alexey <Shestakov.a.mail.work@gmail.com>
2021-01-08 19:18:57 +0200

    added libraries: prop-types formik react-moment Classnames

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import TableHead from "./components/tableHead/tabelHead";
 import TableBody from "./components/tableBody/tableBody";
 import TableFooter from "./components/tableFooter/tableFooter";
 import MonthVacationCounter from "./components/monthVacationCounter/monthVacationCounter";
+import Moment from 'react-moment';
+import PropTypes from 'prop-types';
 
 function App() {
   return (
     <div className="wrapper">
+        <Moment/>
       <MonthSwitcher/>
       <div className="table-wrapper">
         <TableHead/>
