fa5bedc06364a56f99b7e333fcc10da0678ea0ef
Oksana Kushchik <okushchik@gmail.com>
2021-01-12 13:49:20 +0200

    added vacation to calendar

--- src/components/teamHead/teamHead.js
+++ src/components/teamHead/teamHead.js
@@ -19,7 +19,7 @@ function TeamHead(props) {
         <tr className="department" style={color}>
             <td className="department_inform">
                 <span className="department_title">{props.teamName}</span>
-                <span class="department_countMembersIcon">
+                <span className="department_countMembersIcon">
                     {/* <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g opacity="0.5" clip-path="url(#clip0)">
                         <path
